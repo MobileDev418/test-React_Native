5baa42a2a52937b191ee5c5e8edb1dde11be8e06
Ma Huateng <oncemoreforyou@outlook.com>
2017-12-01 04:52:16 +0200

    update

--- testPro/App.js
+++ testPro/App.js
@@ -5,20 +5,64 @@
  */
 
 import React, { Component } from 'react';
-import { Navigation } from 'react-native-navigation';
-import registerScreens from './screens';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
-registerScreens(store, Provider);
+import MainScreen from "./src/modules/index.js";
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
 
 export default class App extends Component {
-
-  constructor(props) {
-    super(props)
-    this.runApp();
+  constructor() {
+    super();
+    this.state = {
+      isReady: false
+    };
   }
-  runApp() {
-      Navigation.startSingleScreenApp({
-          screen: { screen: 'test.SplashScreen' },
-      });
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isReady: true });
+    }, 3000);
+  }
+
+  render() {
+    if (this.state.isReady) {
+      return <MainScreen />;
+    }
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Hello World!
+        </Text>
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
